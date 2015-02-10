56a51b8102873011698dc54771fcde353c64a735
icharge <charge_n@hotmail.com>
2015-02-10 17:56:37 +0700

    Update DB

--- db.sql
+++ db.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jan 06, 2015 at 08:40 AM
+-- Generation Time: Feb 10, 2015 at 11:56 AM
 -- Server version: 5.6.20
 -- PHP Version: 5.5.15
 
@@ -40,6 +40,7 @@ INSERT INTO `products` (`product_barcode`, `name`, `netcontent`) VALUES
 ('1234567890123', 'Test Product', '123 ml'),
 ('1234567890124', 'Test Product 2', '123 ml'),
 ('2000000355429', 'Big C ไข่ไก่เบอร์ 2', '10 Pcs'),
+('32324241312', 'สินค้า 1', '200 ml'),
 ('8850188242103', 'Foremost UHT รสจืด', '180 ml'),
 ('8850188250207', 'Foremost UHT รสหวาน', '225 ml'),
 ('8850188800198', 'Foremost Falcon', '1000 ml'),
@@ -63,37 +64,37 @@ CREATE TABLE IF NOT EXISTS `slots` (
 --
 
 INSERT INTO `slots` (`slot_barcode`, `slot_name`) VALUES
-('EGGS01', 'Eggs A1'),
-('EGGS02', 'Eggs A2'),
-('EGGS03', 'Eggs A3'),
-('EGGS04', 'Eggs A4'),
-('EGGS05', 'Eggs A5'),
-('EGGS06', 'Eggs B1'),
-('EGGS07', 'Eggs B2'),
-('EGGS08', 'Eggs B3'),
-('EGGS09', 'Eggs B4'),
-('EGGS10', 'Eggs B5'),
-('FREEZER', 'Freeze'),
-('MEAT', 'Meat'),
-('MILK01', 'Milk 200-1'),
-('MILK02', 'Milk 200-2'),
-('MILK03', 'Milk 200-3'),
-('MILK04', 'Milk 400-1'),
-('MILK05', 'Milk 400-2'),
-('MILK06', 'Milk 400-3'),
-('MILK07', 'Milk 830-1'),
-('MILK08', 'Milk 830-2'),
-('MILK09', 'Milk 830-3'),
-('OTHER1', 'Other 1'),
-('OTHER2', 'Other 2'),
-('OTHER3', 'Other 3'),
-('SOFTDRK01', 'Soft Drink 1'),
-('SOFTDRK02', 'Soft Drink 2'),
-('SOFTDRK03', 'Soft Drink 3'),
-('VEGETABLE', 'Vegetable'),
-('WATER01', 'Water 1'),
-('WATER02', 'Water 2'),
-('WATER03', 'Water 3');
+('A1', 'A1'),
+('A2', 'A2'),
+('A3', 'A3'),
+('A4', 'A4'),
+('A5', 'A5'),
+('A6', 'A6'),
+('A7', 'A7'),
+('B1', 'B1'),
+('B2', 'B2'),
+('B3', 'B3'),
+('B4', 'B4'),
+('B5', 'B5'),
+('B6', 'B6'),
+('B7', 'B7'),
+('C1', 'C1'),
+('C2', 'C2'),
+('C3', 'C3'),
+('D1', 'D1'),
+('D2', 'D2'),
+('D3', 'D3'),
+('D4', 'D4'),
+('D5', 'D5'),
+('E1', 'E1'),
+('E2', 'E2'),
+('E3', 'E3'),
+('E4', 'E4'),
+('FREEZE', 'FREEZE'),
+('MEAT', 'MEAT'),
+('OTHER1', 'OTHER1'),
+('OTHER2', 'OTHER2'),
+('VEGETABLE', 'Vegetable');
 
 -- --------------------------------------------------------
 
@@ -112,10 +113,10 @@ CREATE TABLE IF NOT EXISTS `slot_detail` (
 --
 
 INSERT INTO `slot_detail` (`slot_barcode`, `product_barcode`, `expire_date`) VALUES
-('EGGS01', '2000000355429', '2014-12-28'),
-('MILK01', '8850188242103', '2015-01-03'),
-('MILK02', '8850188250207', '2015-01-04'),
-('MILK03', '8851013781491', '2015-01-15');
+('A1', '2000000355429', '2015-01-22'),
+('D4', '8850188242103', '2015-01-03'),
+('D5', '8850188250207', '2015-01-04'),
+('D3', '8851013781491', '2015-01-15');
 
 --
 -- Indexes for dumped tables
