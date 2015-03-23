8c309246dfe11e3e9cb63ab6e037a67f7b41a36a
icharge <charge_n@hotmail.com>
2015-03-23 22:57:54 +0700

    Add Send mail

---
+++ function_sendmail.php
@@ -0,0 +1,36 @@
+<?php
+
+require_once('mailer/class.phpmailer.php');
+
+function sendEmail($content) {
+
+	$mail = new PHPMailer();
+	$mail->IsHTML(true);
+	$mail->IsSMTP();
+	$mail->SMTPAuth = true; // enable SMTP authentication
+	$mail->SMTPSecure = "tls"; // sets the prefix to the servier
+	$mail->SMTPDebug = 0; // Debugging msg
+
+	$mail->CharSet = 'UTF-8'; // Support Thai language
+
+	$mail->Host = "smtp.gmail.com"; // sets GMAIL as the SMTP server
+	$mail->Port = 587; // set the SMTP port for the GMAIL server
+	$mail->Username = "charge01.n@gmail.com"; // GMAIL username
+	$mail->Password = 'Charge12425'; // GMAIL password
+	// =======================================================
+	$mail->SetFrom($mail->Username); // From
+	//$mail->AddReplyTo = "support@thaicreate.com"; // Reply
+	$mail->FromName = "Smart Refrigerator";  // set from Name
+	$mail->AddAddress("charge_n@hotmail.com", "Name"); // to Address
+
+
+	$mail->Subject = "Smart Refrigerator"; 
+	$mail->Body = $content;
+
+	if (!$mail->Send()) {
+		echo "Error: $mail->ErrorInfo";
+		return false;
+	} else {
+		return true;
+	}
+}
---
+++ sendmail.php
@@ -0,0 +1,44 @@
+<?php
+	require_once('autoload.php');
+	include('function_sendmail.php');
+
+	$sql = "SELECT sd.slot_barcode, slot_name, sd.product_barcode, name,expire_date, 
+datediff(expire_date,now()) as countexpire
+FROM slot_detail sd
+LEFT JOIN slots s on s.slot_barcode = sd.slot_barcode
+LEFT JOIN products p on p.product_barcode = sd.product_barcode
+WHERE datediff(expire_date,now()) <= ".expire_day_notify."
+ORDER BY datediff(expire_date,now()) asc";
+	$expiringdata = $db->query($sql);
+
+	$refrige = array();
+	$freshboxcolor = array();
+
+	$html = "<h1>รายการเตือนการหมดอายุ</h1>";
+	$html .= '<table border="1">
+						 <tr>
+						 	<th>รหัสสินค้า</th>
+						 	<th>ชั้นวาง</th>
+						 	<th>สินค้า</th>
+						 	<th>สถานะ</th>
+						 </tr>';
+	foreach ($expiringdata as $row) {
+		$refrige[$row['slot_name']] = "active";
+		$freshboxcolor[$row['slot_name']] = expirewarningcolor($row['expire_date']);
+		$colornoti = expirewarningcolor($row['expire_date']);
+		$expiretext = expireremain($row['expire_date']);
+		$html .= "<tr style='$colornoti'>
+				<td>$row[product_barcode]</td>
+				<td>$row[slot_name]</td>
+				<td>$row[name]</td>
+				<td>$expiretext</td>
+			</tr>";
+
+	}
+	$html .= "</table>";
+
+	if (sendEmail($html)) {
+		echo "Mail sent !.";
+	} else {
+		echo "Error !";
+	}
